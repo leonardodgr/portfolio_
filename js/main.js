e3ca6702717ed6a88ee2ba0ff89e202662e612fb
Leonardo <xelsleonardo@gmail.com>
2025-06-04 16:14:11 -0300

    Update files.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 gsap.registerPlugin(ScrollTrigger);
 
-      gsap.to(".a1-logo", {
-        rotation: 360,
-        duration: 2,
-      });
+gsap.from(".a1-logo", {
+  x: -100,
+  scale: 0.5,
+  opacity: 0,
+  duration: 1.5,
+  ease: "back.out(1.7)",
+});
 
-      gsap.fromTo(
-        ".box-2",
-        { x: 600 },
-        { duration: 1.9, x: 0, ease: "elastic.out(1.3, 0.8)" }
-      );
+gsap.fromTo(
+  ".box-2",
+  { x: 600 },
+  { duration: 1.9, x: 0, ease: "elastic.out(1.3, 0.8)" }
+);
 
-      gsap.from("#footer", {
-        scrollTrigger: {
-          trigger: "#footer",
-          start: "top 90%",
-          toggleActions: "play none none none",
-          onEnter: () => console.log("Footer entrou na tela"),
-        },
-        x: -500,
-        opacity: 0,
-        duration: 1.9,
-        ease: "elastic.out(1, 1)",
-      });
+gsap.from("#footer", {
+  scrollTrigger: {
+    trigger: "#footer",
+    start: "top 90%",
+    toggleActions: "play none none none",
+    onEnter: () => console.log("Footer entrou na tela"),
+  },
+  x: -500,
+  opacity: 0,
+  duration: 1.9,
+  ease: "elastic.out(1, 1)",
+});
